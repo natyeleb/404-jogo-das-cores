97113cfe0c1978b5fdce1652ec9b710d96bfa369
natyele <natyelebertolanii@gmail.com>
2019-07-25 17:53:45 -0300

    adicionar jogo

---
+++ README.md
@@ -0,0 +1,20 @@
+# JOGO DAS CORES
+
+## Linguagem utilizadas
+- HTML
+- CSS
+- JAVASCRIPT 
+--
+### Ferramentas das linguagens
+
+- Ultilizamos em `CSS` display flex e propriedades simples de ediçao 
+- Em `Javascript`, utilizamos `FOR`,`MATH` e `FUNCTION`.
+
+##EX: função resetar jogo
+
+~~~
+
+
+
+
+~~~
---
+++ script.js
@@ -0,0 +1,15 @@
+//let numero = Math.round(6.4); //ARRREDONDA PRO MAIS  PROXIMO
+
+//numero = Math.floor(1625.6); // ARREDONDA PRA BAIXO
+
+//numero = Math.ceil(25.5);   // ARREDONDA PRA CIMA
+
+//numero = Math.pow(2, 3);   // NUMERO EXPOENTE
+
+//numero = Math.sqrt(144)    //RAIZ
+
+//numero = Math.pow
+
+//console.log(numero);
+
+let numero = Math.ceil(Math.random()*100);  //GERAR NUMEROS ALEATORIOS PRA CIMA
